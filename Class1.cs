17d46f05ef18b024a51ad871f111687830df1819
abarabone <abara00@live.jp>
2019-04-03 23:23:11 +0900

    下書き

---
+++ Class1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace mmm
+{
+
+	public interface IMmObject<TMm>
+	{
+		TMm AddAttribute( IMmObject obj );
+
+		MmPosition Center { get; }
+	}
+
+
+	public class MmNode : IMmObject<MmNode>
+	{
+		public Node	parent;
+		public Node	child;
+		public Node	sibling;
+		
+		public Node( IMmObject data )
+		{
+
+		}
+
+	}
+
+	public class MmImage : IMmObject<MmImage>
+	{
+
+	}
+
+	public struct MmPosition : IMmObject<MmPosition>
+	{
+		public float	x;
+		public float	y;
+	}
+}
+
